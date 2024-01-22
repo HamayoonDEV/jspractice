140c9e73ac6f801209136c1ff066c4af9ee7a80f
Hamayoon <hamayoon.47611@iqra.edu.pk>
2024-01-22 17:01:28 +0500

    speed checker method logic

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-function fizzBuzz(input) {
-  if (input % 3 === 0 && input % 5 === 0) {
-    return "fizzBuzz";
-  } else if (input % 3 === 0) {
-    return "fizz";
-  } else if (input % 5 === 0) {
-    return "buzz";
-  } else if (typeof input !== "number") {
-    return NaN;
-  }
+//speed limit 70kmh
+//less the or equal to 70 kmh it message should be ok
+// if the speed is greater then 70 add points by 5kmh
 
-  return input;
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+  if (speed < speedLimit + kmPerPoint) {
+    return "ok";
+  } else {
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points > 12) {
+      return "license suspended!";
+    }
+    return points;
+  }
 }
 
-console.log(fizzBuzz(true));
+console.log(checkSpeed(135));
