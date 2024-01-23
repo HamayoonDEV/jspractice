0fe30830b957f9b89f183bdb711ff79cfb4e1d04
Hamayoon <hamayoon.47611@iqra.edu.pk>
2024-01-23 21:05:07 +0500

    even odd method

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-//speed limit 70kmh
-//less the or equal to 70 kmh it message should be ok
-// if the speed is greater then 70 add points by 5kmh
-
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-  if (speed < speedLimit + kmPerPoint) {
-    return "ok";
-  } else {
-    const points = Math.floor((speed - speedLimit) / kmPerPoint);
-    if (points > 12) {
-      return "license suspended!";
+function checkNumber(limit) {
+  for (let i = 1; i <= limit; i++) {
+    if (i % 2 == 0) {
+      console.log(i, "Even");
+    } else {
+      console.log(i, "Odd");
     }
-    return points;
   }
 }
 
-console.log(checkSpeed(135));
+checkNumber(10);
